47bf6ff0d16ab9e6323fafca591df93670f3b8af
Harry! <harryyangzy@gmail.com>
2025-03-25 12:25:29 -0400

    asflkgjhsrlkgjh

--- wsc-vite-app/src/pages/About.jsx
+++ wsc-vite-app/src/pages/About.jsx
@@ -6,13 +6,25 @@ function About() {
     return (
         <>
             <Nav />
-            <div>
+            <div className = "flex flex-col justify-center w-[850px] mx-auto">
                 <h1 className="">About Us</h1>
-            </div>
-            <div className = "flex flex-col justify-center w-[800px] mx-auto">
+            <div >
                 <hr className="border-t-2 border-gold my-10"></hr>
+                <div className="my-20 flex items-center gap-8">
+                    <img src="about.png" width={390} height={300} alt="About image" />
+                    <h4 className="text-left my-1">
+                    Our mission is to promote the sport of weightlifting in the Washington State area.
+                    </h4>
+                </div>
+            </div>
+            <div className = "my-10">
                 <h3 className="text-left text-[#ffd95a]">Mission</h3>
-                <p className="text-left"> Our mission is to promote the sport of weightlifting in the Washington State area.</p>
+                <p className="text-left my-1"> Our mission is to promote the sport of weightlifting in the Washington State area.</p>
+                <h3 className="text-left text-[#ffd95a] mt-5">Vision</h3>
+                <p className="text-left my-1"> Our mission is to promote the sport of weightlifting in the Washington State area.</p>
+                <h3 className="text-left text-[#ffd95a] mt-5">Values</h3>
+                <p className="text-left my-1"> Our mission is to promote the sport of weightlifting in the Washington State area.</p>
+            </div>
             </div>
             <Footer />
         </>
--- wsc-vite-app/src/pages/ExecutiveTeam.jsx
+++ wsc-vite-app/src/pages/ExecutiveTeam.jsx
@@ -6,9 +6,21 @@ function ExecutiveTeam() {
     return (
         <>
             <Nav />
+            <div className="flex flex-col justify-center w-[850px] mx-auto">
+                <h1 className="text-center">Executive Team</h1>
+                <div>
+                    <hr className="border-t-2 border-gold my-10" />
+                    <div className="my-20 flex items-center gap-8 w-[200px]">
+                    <img src="lebron.jpg" className="w-[200px] h-[200px] object-cover" alt="President Headshot" />
+                    <p className="text-left my-1">
+                            Our mission is to promote the sport of weightlifting in the Washington State area.
+                    </p>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </>
-    )
+    );
 }
 
 export default ExecutiveTeam;
