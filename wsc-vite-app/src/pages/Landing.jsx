b0973fdad14f9e751b8dee781f16f30412285c22
Thomas Llamzon <tllamzon@uwo.ca>
2025-05-12 17:49:17 -0700

    Update Landing.jsx

--- wsc-vite-app/src/pages/Landing.jsx
+++ wsc-vite-app/src/pages/Landing.jsx
@@ -38,13 +38,13 @@ function Landing({ events, loading, error }) {
                     <div className="grid md:grid-cols-2 gap-12 items-center">
                     <div className="space-y-6 order-2 md:order-1">
                         <h3 className="text-2xl font-bold">Who We Are</h3>
-                        <p className="text-lg">
+                        <p className="text-lg text-gray-300">
                         Western Sales Club is at the frontier of sales education at Western University.
                         We're not just teaching sales, we're building effective communicators and personable leaders –
                         giving you the confidence to succeed in any field.
                         </p>
                         <h3 className="text-2xl font-bold pt-4">Our Mission</h3>
-                        <p className="text-lg pb-4">
+                        <p className="text-lg  text-gray-300 pb-4">
                             Western Sales Club envisions a future where sales education and representation 
                             is fully integrated into post-secondary learning, equipping students with practical 
                             communication skills, confidence, industry connections, and a supportive community to 
@@ -96,40 +96,63 @@ function Landing({ events, loading, error }) {
                 </div>
 
                 {/* WHAT WE DO */}
-                <div className="w-full pb-12 sm:pb-24 py-8 sm:py-16 flex flex-col items-center px-4">
-                    <h2 className="text-3xl sm:text-4xl font-georgia font-bold pt-4 mb-16">What We Do</h2>
-                    <div className="flex flex-col sm:flex-row justify-center gap-8 sm:gap-32">
-                        <div className="flex flex-col items-center">
-                            <div className="w-24 h-24 sm:w-32 sm:h-32 rounded-full border-4 border-purple flex items-center justify-center mb-4 sm:mb-6 transition-shadow duration-300 hover:shadow-[0_0_30px_#7f42a9]">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 sm:w-16 sm:h-16 text-white">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-                                </svg>
-                            </div>
-                            <p className="text-lg sm:text-xl font-georgia text-center w-40 sm:w-48">
-                                Guest Speaker Panels
-                            </p>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <div className="w-24 h-24 sm:w-32 sm:h-32 rounded-full border-4 border-purple flex items-center justify-center mb-4 sm:mb-6 transition-shadow duration-300 hover:shadow-[0_0_30px_#7f42a9]">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 sm:w-16 sm:h-16 text-white">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
-                                </svg>
-                            </div>
-                            <p className="text-lg sm:text-xl font-georgia text-center w-40 sm:w-48">
-                                Sales Competitions
-                            </p>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <div className="w-24 h-24 sm:w-32 sm:h-32 rounded-full border-4 border-purple flex items-center justify-center mb-4 sm:mb-6 transition-shadow duration-300 hover:shadow-[0_0_30px_#7f42a9]">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 sm:w-16 sm:h-16 text-white">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
-                                </svg>
-                            </div>
-                            <p className="text-lg sm:text-xl font-georgia text-center w-40 sm:w-48">
-                                Strategy Enhancement
-                            </p>
-                        </div>
+                <div className="w-full pb-12 sm:pb-24 py-8 sm:py-16 flex flex-col items-center px-12">
+                <h2 className="text-3xl sm:text-4xl font-georgia font-bold pt-4 mb-4">What We Do</h2>
+                <div className="w-20 h-1 bg-gold mx-auto mb-12"></div>
+
+                <div className="flex flex-col sm:flex-row items-center sm:items-start sm:justify-center gap-8 sm:gap-16 mb-12 max-w-5xl">
+                    <div className="flex-shrink-0">
+                    <div className="w-24 h-24 sm:w-32 sm:h-32 rounded-full border-4 border-purple flex items-center justify-center transition-shadow duration-300 hover:shadow-[0_0_30px_#7f42a9]">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 sm:w-16 sm:h-16 text-white">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                        </svg>
                     </div>
+                    </div>
+                    <div className="text-lg sm:text-xl font-georgia text-white sm:text-left">
+                    <h3 className="font-bold text-2xl mb-4">Guest Speaker Panels</h3>
+                    <p className="text-lg text-gray-300">
+                        Gain direct exposure to accomplished professionals across a variety of industries through our engaging speaker panels. 
+                        Learn about their journeys, challenges, and the sales and communication skills that helped them succeed—offering you both 
+                        inspiration and actionable advice.
+                    </p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col sm:flex-row-reverse items-center sm:items-start sm:justify-center gap-8 sm:gap-16 mb-12 max-w-5xl">
+                    <div className="flex-shrink-0">
+                    <div className="w-24 h-24 sm:w-32 sm:h-32 rounded-full border-4 border-purple flex items-center justify-center transition-shadow duration-300 hover:shadow-[0_0_30px_#7f42a9]">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 sm:w-16 sm:h-16 text-white">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                        </svg>
+                    </div>
+                    </div>
+                    <div className="text-lg sm:text-xl font-georgia text-white sm:text-right">
+                    <h3 className="font-bold text-2xl mb-4">Sales Competitions</h3>
+                    <p className="text-lg text-gray-300">
+                        Our sales competitions are a fun, energizing way to learn by doing. 
+                        Whether you’re brand new or experienced, you’ll enjoy role-playing real-world scenarios, 
+                        refining your pitch, and receiving feedback from judges—all while building confidence and making new connections.
+                    </p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col sm:flex-row items-center sm:items-start sm:justify-center gap-8 sm:gap-16 max-w-5xl">
+                    <div className="flex-shrink-0">
+                    <div className="w-24 h-24 sm:w-32 sm:h-32 rounded-full border-4 border-purple flex items-center justify-center transition-shadow duration-300 hover:shadow-[0_0_30px_#7f42a9]">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 sm:w-16 sm:h-16 text-white">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
+                        </svg>
+                    </div>
+                    </div>
+                    <div className="text-lg sm:text-xl font-georgia text-white sm:text-left">
+                    <h3 className="font-bold text-2xl mb-4">Strategy Enhancement</h3>
+                    <p className="text-lg text-gray-300">
+                        Western Sales Club partners with actual organizations and businesses to offer students the opportunity 
+                        to apply sales strategies in meaningful, real-world projects. These experiences help develop your 
+                        problem-solving, communication, and strategic thinking in a professional context.
+                    </p>
+                    </div>
+                </div>
                 </div>
 
                 <hr className="mx-auto w-2/3 py-14 border-1 border-[#F9C726]" />
